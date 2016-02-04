cb47297fae99e8a00a497b45bc74e16abb6d5c0d
Erin Mounts <erinys62@gmail.com>
2016-02-04 10:54:53 -0800

    Initial commit

---
+++ BridgeAppSDK/BridgeAppSDK.h
@@ -0,0 +1,19 @@
+//
+//  BridgeAppSDK.h
+//  BridgeAppSDK
+//
+//  Created by Erin Mounts on 1/28/16.
+//  Copyright © 2016 Apple, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BridgeAppSDK.
+FOUNDATION_EXPORT double BridgeAppSDKVersionNumber;
+
+//! Project version string for BridgeAppSDK.
+FOUNDATION_EXPORT const unsigned char BridgeAppSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BridgeAppSDK/PublicHeader.h>
+
+
---
+++ BridgeAppSDKSample/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  BridgeAppSDK
+//
+//  Created by Erin Mounts on 1/28/16.
+//  Copyright © 2016 Apple, Inc. All rights reserved.
+//
+
+import UIKit
+import BridgeAppSDK
+
+@UIApplicationMain
+
+class AppDelegate: BridgeAppSDK.AppDelegate {
+}
