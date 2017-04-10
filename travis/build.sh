cdafed26c0055d9ae51ed3414db16605de269fe8
Khai Do <zaro0508@gmail.com>
2017-04-10 15:09:56 -0700

    Use bundler and a Gemfile to manage dependencies
    Use bundler with a Gemfile is recommended[1] for managing fastlane
    dependencies.
    
    [1] https://docs.fastlane.tools/getting-started/ios/setup/

--- travis/build.sh
+++ travis/build.sh
@@ -3,8 +3,8 @@ set -ex
 # show available schemes
 # xcodebuild -list -project ./BridgeAppSDK.xcodeproj
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    fastlane test scheme:"BridgeAppSDK"
+    bundle exec fastlane test scheme:"BridgeAppSDK"
 elif [ "$TRAVIS_BRANCH" = "master" ]; then
-    fastlane ci_archive scheme:"BridgeAppSDKSample" export_method:"enterprise"
+    bundle exec fastlane ci_archive scheme:"BridgeAppSDKSample" export_method:"enterprise"
 fi
 exit $?
