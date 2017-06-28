2c29b61cbe6602e7205911fe72e8e1b341d4a602
Erin Mounts <erinys62@gmail.com>
2017-06-28 15:13:30 -0700

    Clear keychain on delete/re-install

--- BridgeAppSDK/SBAUser.swift
+++ BridgeAppSDK/SBAUser.swift
@@ -41,7 +41,18 @@ import ResearchKit
  */
 public final class SBAUser: NSObject, SBAUserWrapper, SBANameDataSource, SBBAuthManagerDelegateProtocol {
 
-    static let shared = SBAUser()
+    static let shared: SBAUser = {
+        let user = SBAUser()
+        // If a previous install on this device was deleted, its keychain will still be around, but its
+        // userDefaults will have been wiped. In that case we want to make sure the old keychain is wiped
+        // so BridgeSDK doesn't see the old credentials and try to read/write from the old account during
+        // the onboarding/consent/signUp process.
+        if user.onboardingStepIdentifier == nil &&
+            !user.isLoginVerified {
+            user.resetStoredUserData()
+        }
+        return user
+    }()
     
     lazy var profileManager: SBAProfileManagerProtocol? = {
         return SBAProfileManager.shared
