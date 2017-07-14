3479501c178d9e531f52d1d0e9d673bfedc7416b
Erin Mounts <erinys62@gmail.com>
2017-07-13 22:00:48 -0700

    cleanup

--- BridgeAppSDK/SBAProfileItem.swift
+++ BridgeAppSDK/SBAProfileItem.swift
@@ -838,9 +838,7 @@ open class SBAClientDataProfileItem: SBAProfileItemBase {
         didSet {
             // get all the SBAClientDataProfileItem instances from SBAProfileManager
             guard scheduledActivities != nil && scheduledActivities!.count > 0,
-                    let clientDataItems: [SBAClientDataProfileItem] = SBAProfileManager.shared?.profileItems().values.mapAndFilter({ (profileItem) -> SBAClientDataProfileItem? in
-                    return profileItem as? SBAClientDataProfileItem
-                }) else { return }
+                    let clientDataItems: [SBAClientDataProfileItem] = SBAProfileManager.shared?.profileItems().values.mapAndFilter({ return $0 as? SBAClientDataProfileItem }) else { return }
             
             var updatedDemographicData = false
             for item in clientDataItems {
